4fa8cdca2e34ffad29c97ca31e7d2a172d6a0c7a
Rees <jiey@kean.edu>
2022-02-25 20:35:52 +0800

    Create in class 1.py

---
+++ in class 1.py
@@ -0,0 +1,14 @@
+name = input("Name：")
+name = str(name)
+age = input("Age:")
+age = int(age)
+address = input("Address:")
+address = str(address)
+FSport = input("Favorite sport:")
+FSport = str(FSport)
+print ("Hello, my name is:", name);
+print("I am ", age,"years");
+print("I live in",address);
+print("I like to play",FSport);
+
+#Team member:Zhao Dong (Von), 1098120; Yang Jie(Ress), 1098564
